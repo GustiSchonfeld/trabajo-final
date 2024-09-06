448fb97e2a2a2c5f657b1d9af327fee9473d411c
GustiSchonfeld <54253264+GustiSchonfeld@users.noreply.github.com>
2024-09-06 00:05:18 -0300

    Pantalla principal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import React from 'react';
 import './App.css';
+import { FaUser, FaBriefcase, FaBook, FaHeartbeat, FaGraduationCap, FaHandsHelping, FaPhoneAlt, FaEnvelope } from 'react-icons/fa';
+import { BsWhatsapp } from 'react-icons/bs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Mi Proyecto Final</h1>
+    <div className="container">
+      <header className="header">
+        <img src="images/logoSJM.svg" alt="Logo" className="logo" />
+        <h1>integrAR</h1>
+        <button className="menu-btn">
+          <span className="menu-icon">&#9776;</span>
+        </button>
       </header>
-      <main>
-        <section>
-          <h2>Bienvenido a mi proyecto</h2>
-          {/* Aquí puedes agregar más secciones y componentes */}
-        </section>
-      </main>
-      <footer className="App-footer">
-        <p>© 2024 Mi Proyecto</p>
+
+      <div className="grid-container">
+        <div className="grid-item">
+          <FaUser size={50} />
+          <p>Trámites y Documentos</p>
+        </div>
+        <div className="grid-item">
+          <FaHeartbeat size={50} />
+          <p>Salud</p>
+        </div>
+        <div className="grid-item">
+          <FaBook size={50} />
+          <p>Educación</p>
+        </div>
+        <div className="grid-item">
+          <FaBriefcase size={50} />
+          <p>Busco Trabajo</p>
+        </div>
+        <div className="grid-item">
+          <FaHandsHelping size={50} />
+          <p>Asistencia</p>
+        </div>
+        <div className="grid-item">
+          <FaGraduationCap size={50} />
+          <p>Capacitaciones</p>
+        </div>
+      </div>
+
+      <footer className="footer">
+        <p><FaPhoneAlt /> 1128478764</p>
+        <p><FaEnvelope /> contacto@sjmargentina.org</p>
+        <BsWhatsapp size={30} className="whatsapp-icon" />
       </footer>
     </div>
   );
 }
 
 export default App;
+
